c50800832e66ae9d4c95832ebe643547b933555c
Vu Truong Giang <vutruonggiangty@gmail.com>
2026-06-19 15:17:17 +0700

    multi language cho interceptor xu ly loi

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { globalHttpErrorInterceptor } from './core/interceptors/global-http-error.interceptor';
+
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +19,14 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         tokenInterceptor,
         globalHttpErrorInterceptor
-      ]))
+      ])),
+    provideTranslateService({
+      lang: 'vi',
+      fallbackLang: 'vi',
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      })
+    })
   ]
 };
--- src/app/core/interceptors/global-http-error.interceptor.ts
+++ src/app/core/interceptors/global-http-error.interceptor.ts
@@ -3,21 +3,24 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
+import { TranslateService } from '@ngx-translate/core';
+
 export const globalHttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const toast = inject(ToastrService)
+  const translate = inject(TranslateService)
   return next(req).pipe(
     catchError((e:HttpErrorResponse) => {
       switch (e.status){
         case 401:
-          toast.error("Bạn chưa đăng nhập hoặc token hết hạn", "lỗi 401");
+          toast.error(translate.instant('ERROR.401'));
           localStorage.removeItem('accessToken');
           window.location.href = '/login';
           break;
         case 403: 
-          toast.warning("Bạn không đủ quyền truy cập", "Lỗi 403");
+          toast.warning(translate.instant('ERROR.403'));
           break;
         default:
-          toast.error("Có lỗi xảy ra", "lỗi");
+          toast.error(translate.instant('ERROR.DEFAULT'));
           break;
       }
       return throwError(() => e);
