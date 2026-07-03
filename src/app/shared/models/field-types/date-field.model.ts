313f86e8d1703caf944514ebd10391d35f69a27d
Vu Truong Giang <vutruonggiangty@gmail.com>
2026-07-03 14:18:53 +0700

    add

---
+++ src/app/shared/models/field-types/date-field.model.ts
@@ -0,0 +1,8 @@
+import { BaseFieldConfig } from './../base-field-config.model';
+
+export interface DateField extends BaseFieldConfig {
+    type: 'date';
+
+    minDate?: Date;
+    maxDate?: Date;
+}
